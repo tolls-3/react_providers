bc559ef22c374be4b6ed2e58a28bdc70daf0313a
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-07-11 14:41:47 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.scss';
 import {
   BrowserRouter,
   Switch,
-  Route
+  Route, NavLink
 } from 'react-router-dom';
 import ExplorePage from './layouts/ExplorePage';
 import PageNotFound from './layouts/PageNotFound';
+import ViewProvider from "./layouts/ViewProvider"
 
 function App() {
   return (
     <BrowserRouter history="">
       <Switch>
-        <Route path="/" exact component={ExplorePage}/>
+        <Route path="/" exact component={ExplorePage} />
         {/* TODO (6a): Add New Route for Viewing a single Provider */}
+        <Route path="/providers/:id" exact component={ViewProvider} />
         <Route component={PageNotFound} />
       </Switch>
     </BrowserRouter>
--- src/layouts/ViewProvider.js
+++ src/layouts/ViewProvider.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ApiService from '../utils/apiService';
+import ProviderCard from '../components/ProviderCard';
 
-const ViewProvider = () => (
+const ViewProvider = (props) => {
   // TASK 6:
   // Render Single Provider View Here
   // Feel free to using existing styles,
@@ -8,8 +10,45 @@ const ViewProvider = () => (
   //
   // For Bonus points, you can also add functionality to edit the provider
   // Reusing the NewProviderForm component for this will make it a whole lot easier :D
+  const [provider, setProvider] = useState("")
+  useEffect(() => {
+    const { id } = this.props.match.params;
+    const url = ApiService.ENDPOINTS.providers
+    ApiService.get(`${url}/${id}`)
+      .then((data) => {
+        setProvider(data.data)
+      });
 
-  <h1>View Provider <span><i className="fa fa-edit"></i></span></h1>
-);
+  }, [])
+
+  function handleClick(){}
+
+  function displayProvider() {
+    let display;
+    if (!provider) {
+      display = <p>Error getting Provider</p>
+    } else {
+      display = (
+        <ProviderCard
+          key={provider.id}
+          name={provider.name}
+          address={provider.location.address}
+          rating={provider.rating}
+          providerType={provider.type}
+          imageUrl={provider.imageUrl}
+          onClick={() => handleClick(provider.id)}
+        />
+      )
+    }
+
+    return display;
+  }
+
+  return (
+
+    <h1>View Provider <span><i className="fa fa-edit">{displayProvider()}</i></span></h1>
+  )
+
+}
 
 export default ViewProvider;
